0772e89dd0b62afbfa983f83fb89a3a52347d32d
Lans12345 <olanalans12345@gmail.com>
2024-03-28 07:38:24 +0800

    added chat ui

--- lib/screens/tabs/chats/classes_tab.dart
+++ lib/screens/tabs/chats/classes_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:school_systems/screens/tabs/chats/chat_page.dart';
 import 'package:school_systems/utlis/colors.dart';
 import 'package:school_systems/widgets/text_widget.dart';
 
@@ -11,61 +12,67 @@ class ClassesTab extends StatelessWidget {
       itemBuilder: (context, index) {
         return Padding(
           padding: const EdgeInsets.only(top: 10, bottom: 10),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              SizedBox(
-                height: 50,
-                child: VerticalDivider(
-                  color: secondary,
-                  thickness: 3,
-                  width: 3,
+          child: GestureDetector(
+            onTap: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => ChatPage()));
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 50,
+                  child: VerticalDivider(
+                    color: secondary,
+                    thickness: 3,
+                    width: 3,
+                  ),
                 ),
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              CircleAvatar(
-                minRadius: 27,
-                maxRadius: 27,
-                backgroundColor: grey,
-              ),
-              const SizedBox(
-                width: 20,
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextWidget(
-                    text: 'MATH 101',
-                    fontSize: 18,
-                    fontFamily: 'Bold',
-                    color: primary,
-                  ),
-                  SizedBox(
-                    width: 175,
-                    child: TextWidget(
-                      text: 'Jobert: Naay Klase Karon?',
-                      fontSize: 14,
-                      fontFamily: 'Regular',
-                      color: grey,
-                    ),
-                  ),
-                ],
-              ),
-              const Expanded(
-                child: SizedBox(
+                const SizedBox(
                   width: 20,
                 ),
-              ),
-              TextWidget(
-                text: '10:00 AM',
-                fontSize: 12,
-                fontFamily: 'Regular',
-                color: grey,
-              ),
-            ],
+                CircleAvatar(
+                  minRadius: 27,
+                  maxRadius: 27,
+                  backgroundColor: grey,
+                ),
+                const SizedBox(
+                  width: 20,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextWidget(
+                      text: 'MATH 101',
+                      fontSize: 18,
+                      fontFamily: 'Bold',
+                      color: primary,
+                    ),
+                    SizedBox(
+                      width: 175,
+                      child: TextWidget(
+                        text: 'Jobert: Naay Klase Karon?',
+                        fontSize: 14,
+                        fontFamily: 'Regular',
+                        color: grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const Expanded(
+                  child: SizedBox(
+                    width: 20,
+                  ),
+                ),
+                TextWidget(
+                  text: '10:00 AM',
+                  fontSize: 12,
+                  fontFamily: 'Regular',
+                  color: grey,
+                ),
+              ],
+            ),
           ),
         );
       },
